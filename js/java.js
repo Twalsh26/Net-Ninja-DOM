4e49779c8ddcad0b4eab1aceb42a4b6f759298b4
Thomas W <126Twalsh@frhsd.com>
2024-11-21 13:38:28 -0500

    Leason7

--- js/java.js
+++ js/java.js
@@ -1,9 +1,32 @@
-const banner = document.querySelector('#page-banner');
+// Leason 6
+// const banner = document.querySelector('#page-banner');
 
-console.log('#page-banner node type is:', banner.nodeType);
-console.log('#page-banner node name is:', banner.nodeName);
-console.log('#page-banner has child nodes:', banner.hasChildNodes());
+// console.log('#page-banner node type is:', banner.nodeType);
+// console.log('#page-banner node name is:', banner.nodeName);
+// console.log('#page-banner has child nodes:', banner.hasChildNodes());
 
-const clonedBanner = banner.cloneNode(true);
-console.log(clonedBanner);
-// test 
+// const clonedBanner = banner.cloneNode(true);
+// console.log(clonedBanner);
+
+//Leason 7
+const bookList = document.querySelector('#book-list');
+
+console.log('book list parent element:', bookList.parentElement);
+console.log('book list parent node:', bookList.parentNode);
+
+console.log('all node children:');
+Array.from(bookList.childNodes).forEach(function(node){
+  console.log(node);
+});
+
+console.log('all element children:');
+Array.from(bookList.children).forEach(function(node){
+  console.log(node);
+});
+
+const titles = bookList.querySelectorAll('.name');
+
+console.log('Book titles:');
+Array.from(titles).forEach(function(title){
+  console.log(title.textContent);
+});
