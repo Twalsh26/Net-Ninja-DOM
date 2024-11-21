738f33626346f66ad9f8f09d270d57a3aaa00554
Thomas W <126Twalsh@frhsd.com>
2024-11-21 13:51:10 -0500

    9-10 Leason

--- js/java.js
+++ js/java.js
@@ -39,4 +39,35 @@
 // console.log('#book-list previous element sibling:', bookList.previousElementSibling);
 
 // bookList.previousElementSibling.querySelector('p').innerHTML += '<br />Too cool for everyone else!';
+//Leason 9
+// const listItems = document.querySelectorAll('#book-list ul li');
 
+// Array.from(listItems).forEach(function(item){
+//   item.addEventListener('click', (e) => {
+
+//     const li = e.target.parentElement;
+//     console.log('child element to remove:', li);
+//     console.log('parent element to remove child from:', li.parentElement);
+//     li.parentNode.removeChild(li);
+
+//   });
+// });
+
+// // prevent default behaviour
+
+// const link = document.querySelector('#page-banner a');
+
+// link.addEventListener('click', function(e){
+//   e.preventDefault();
+//   console.log('Navigation to', e.target.textContent, 'was prevented');
+// });
+//Leason 10
+// const list = document.querySelector('#book-list ul');
+
+// // delete books
+// list.addEventListener('click', (e) => {
+//   if(e.target.className == 'delete'){
+//     const li = e.target.parentElement;
+//     li.parentNode.removeChild(li);
+//   }
+// });
